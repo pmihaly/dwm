1207ed09deee580cba8233627a28907bf84e646c
pmihaly <pappmihalyemil@protonmail.com>
2020-06-08 12:33:01 +0200

    master-stack szimbólum legyen megjelenítve

--- config.h
+++ config.h
@@ -41,7 +41,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "",      tile },    /* first entry is default */
+	{ "[]=",      tile },    /* first entry is default */
 };
 
 /* key definitions */
