39838f9f768b6072d36347cc1be6e8e1dcfa51d3
pmihaly <pappmihalyemil@protonmail.com>
2021-04-05 19:37:52 +0200

    Run not pretty dmenu

--- config.h
+++ config.h
@@ -54,7 +54,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run_pretty", "-m", dmenumon, "-fn", dmenufont};
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont};
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
